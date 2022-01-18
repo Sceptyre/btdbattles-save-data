e0f7974c8052fd28ab5ddcef640e40aea3d84760
Sceptyre <61129849+Sceptyre@users.noreply.github.com>
2022-01-18 17:23:04 -0500

    Update updateSave.ps1

--- updateSave.ps1
+++ updateSave.ps1
@@ -1,5 +1,5 @@
 $dir=(@('HKLM:\SOFTWARE\Wow6432Node\Valve\Steam','HKLM:\SOFTWARE\Valve\Steam') | Select -First 1 | ? {Test-Path $_} | Get-ItemPropertyValue -Name InstallPath) + "\userdata\166750487\444640\local\Data\Docs";
-Write-Host "Located Steam Directory: $dir";
+Write-Host "Located BTDBattles Steam Directory: $dir";
 
 Write-Host "Renaming Old Profile.save";
 Move-Item -Path "$dir\Profile.save" -Destination "$dir\Profile.save.bak" -Force;
